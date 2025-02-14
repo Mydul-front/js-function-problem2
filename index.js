ff361990d3a3b81dd8b80adc94e1a42361151ca4
Mydul Islam <si240147@gmail.com>
2025-02-14 23:05:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// two number in the getar then solved
+
+/* function getMax(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+const numbers = getMax(5, 7);
+const number1 = getMax(5, 12);
+const number2 = getMax(45, 7);
+const number3 = getMax(5, 71);
+
+console.log("The max number is :", numbers, number1, number2, number3); */
+
+// 3 ta songkhar moddhe kon ta boro ata kmne nirnnoy korbo
+
+/* function threeNumber(num1, num2, num3) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num3) {
+    return num2;
+  } else {
+    return num3;
+  }
+}
+const numbers = threeNumber(12, 34, 190);
+console.log("The Maximum Number is :", numbers); */
+
+// akti array er theke max number nirnoy
+
+/* function menHeight(numbers) {
+  let max = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    // console.log(numbers[i]);
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
+}
+const totalHeight = [65, 76, 98, 90, 50];
+const height = menHeight(totalHeight);
+console.log(height); */
+
+
